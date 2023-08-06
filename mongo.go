955d7ff331b60ddc57101fda4353515bd71e95eb
Chris Mayer <chris.mayer@gmail.com>
2023-08-06 15:12:41 +1000

    Initial commit

---
+++ mongo.go
@@ -0,0 +1,65 @@
+package gomongo
+
+import (
+	"context"
+	"fmt"
+	"net/url"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+// MongoDB is a wrapper around a mongo connection
+type MongoDB struct {
+	connStr string
+	client  *mongo.Client
+}
+
+// NewMongoDB creates a new Mongo clent
+func NewMongoDB(ctx context.Context, server string, user string, password string, port int) (db *MongoDB, e error) {
+	e = nil
+
+	db = new(MongoDB)
+	db.connStr = "mongodb"
+	if port == 0 {
+		db.connStr += "+srv"
+	}
+	db.connStr += "://"
+
+	if user != "" {
+		db.connStr += url.PathEscape(user)
+
+		if password != "" {
+			db.connStr += ":" + url.PathEscape(password) + "@"
+		}
+	}
+
+	db.connStr += server
+	if port != 0 {
+		db.connStr = fmt.Sprintf("%s:%d/", db.connStr, port)
+	}
+
+	clientOptions := options.Client().ApplyURI(db.connStr)
+
+	db.client, e = mongo.Connect(ctx, clientOptions)
+
+	if e == nil {
+		e = db.client.Ping(ctx, nil)
+	} else {
+		fmt.Printf("Error connecting to MongoDB\n")
+	}
+
+	return
+}
+
+func (m *MongoDB) Collection(dbName string, collectionName string) (col *mongo.Collection) {
+	col = m.client.Database(dbName).Collection(collectionName)
+
+	return
+}
+
+func (m *MongoDB) Client() (client *mongo.Client) {
+	client = m.client
+
+	return
+}
---
+++ repository.go
@@ -0,0 +1,109 @@
+package gomongo
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+type Repository[T any] struct {
+	dbClient   *MongoDB
+	dbName     string
+	collection string
+}
+
+type RepositoryCallback[T any] func(ctx context.Context, n *T) (e error)
+
+func NewRepository[T any](ctx context.Context, server string, user string, password string, port int, dbName string, coll string) (r *Repository[T], e error) {
+	r = new(Repository[T])
+
+	r.dbClient, e = NewMongoDB(ctx, server, user, password, port)
+	r.dbName = dbName
+	r.collection = coll
+
+	return
+}
+
+func (r Repository[T]) GetByID(ctx context.Context, ID primitive.ObjectID) (entity *T, e error) {
+	col := r.dbClient.Collection(r.dbName, r.collection)
+
+	filter := bson.D{
+		primitive.E{Key: "_id", Value: ID},
+	}
+
+	e = col.FindOne(ctx, filter).Decode(&entity)
+
+	return
+}
+
+func (r Repository[T]) GetOne(ctx context.Context, key string, value interface{}) (entity *T, e error) {
+	col := r.dbClient.Collection(r.dbName, r.collection)
+
+	filter := bson.D{
+		primitive.E{Key: key, Value: value},
+	}
+
+	e = col.FindOne(ctx, filter).Decode(&entity)
+
+	return
+}
+
+func (r Repository[T]) GetByDateRange(ctx context.Context, key string, start time.Time, end time.Time) (entities []T, e error) {
+	col := r.dbClient.Collection(r.dbName, r.collection)
+
+	filter := bson.M{
+		key: bson.M{
+			"$gte": start,
+			"$lte": end,
+		},
+	}
+
+	cursor, e := col.Find(ctx, filter)
+	if e == nil {
+		e = cursor.All(ctx, &entities)
+	}
+
+	return
+}
+
+func (r Repository[T]) Iterate(ctx context.Context, cb RepositoryCallback[T]) (e error) {
+	col := r.dbClient.Collection(r.dbName, r.collection)
+
+	cursor, err := col.Find(ctx, bson.D{})
+	if err != nil {
+		e = err
+	} else {
+		for cursor.Next(ctx) {
+			var entity T
+			if err := cursor.Decode(&entity); err != nil {
+				e = err
+			} else {
+				e = cb(ctx, &entity)
+			}
+
+		}
+		if err := cursor.Err(); err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	return
+}
+
+func (r Repository[T]) Save(ctx context.Context, entity *T, key string, value interface{}) (e error) {
+	col := r.dbClient.Collection(r.dbName, r.collection)
+
+	query := bson.M{
+		key: value,
+	}
+
+	opts := options.Replace().SetUpsert(true)
+
+	_, e = col.ReplaceOne(ctx, query, *entity, opts)
+
+	return
+}
